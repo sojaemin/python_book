bfa22876929429e4f4e2a0db9e2fe14e1653f93c
sojaemin <jaemin1204@naver.com>
2013-12-09 15:48:05 +0900

    modify string.py

--- String.py
+++ String.py
@@ -18,4 +18,16 @@ print(a[0:4])
 print(a[:4])
 print(a[4:])
 
-print("test")
+#String formating
+print("test string formating %s" % "test")
+
+#Number formating
+print("test number formating %d" % 100)
+
+#more than 2 value formating
+number = 500
+str_val = "five hundred"
+print("sample code with two value A: %d, B: %s" %(number, str_val))
+
+#print %
+print("print fifty percent : %d%%" %50)
