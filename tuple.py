7ae22972c01b1e7c81400199c24fb8b2ad561fbb
sojaemin <jaemin1204@naver.com>
2013-12-10 18:25:53 +0900

    tuple's done

---
+++ tuple.py
@@ -0,0 +1,12 @@
+#tuple : can not midify. can not delete
+tuple1 = ()
+tuple2 = ('test str',)
+tuple3 = 'test str','test str2'
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+print(tuple3[1])
+
+print(tuple2 + tuple3)
+print(tuple3*3)
